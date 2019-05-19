83d8fd3d81c0a584dcd92f96a178f25ff41f949c
Álvaro Molina <abusmolina@gmail.com>
2019-05-18 21:55:18 -0400

    FIX No funcionaba algoritmo de seleeccion de flash_type, se cambio por algo simple

--- app/controllers/application_helper.rb
+++ app/controllers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
-  BOOTSTRAP_FLASH_MSG = {
-    success: 'alert-success',
-    error: 'alert-error',
-    alert: 'alert-block',
-    notice: 'alert-info'
-  }
   def bootstrap_class_for_flash(flash_type)
-    BOOTSTRAP_FLASH_MSG.fetch(flash_type, flash_type.to_s)
+    case flash_type
+    when 'success'
+      'alert-success'
+    when 'error'
+      'alert-danger'
+    when 'alert'
+      'alert-warning'
+    when 'notice'
+      'alert-info'
+    else
+      flash_type.to_s
+    end
   end
 end
